ec3c689a1fc2e011bed059998bba2dbff89910f6
Arrival692 <aagmanbharti71@gmail.com>
2024-10-19 19:45:57 +0530

    Create insertNodeInLL.c

---
+++ insertNodeInLL.c
@@ -0,0 +1,28 @@
+// Given a node prev_node, insert a 
+// new node after the given prev_node 
+void insertAfter(struct Node* prev_node, 
+				int new_data) 
+{ 
+	// 1. Check if the given prev_node 
+	// is NULL 
+	if (prev_node == NULL) 
+	{ 
+	printf("the given previous node cannot be NULL");	 
+	return; 
+	} 
+		
+	// 2. Allocate new node 
+	struct Node* new_node = 
+		(struct Node*) malloc(sizeof(struct Node)); 
+
+	// 3. Put in the data 
+	new_node->data = new_data; 
+
+	// 4. Make next of new node as next 
+	// of prev_node 
+	new_node->next = prev_node->next; 
+
+	// 5. Move the next of prev_node 
+	// as new_node 
+	prev_node->next = new_node; 
+}
